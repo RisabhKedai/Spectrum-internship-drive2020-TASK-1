bd483a3236f3797d4e233bd6b592d8d1e7378019
RisabhKedai <58504602+RisabhKedai@users.noreply.github.com>
2020-05-18 00:51:54 +0530

    Update task 1.11p.py

--- task 1.11p.py
+++ task 1.11p.py
@@ -1,7 +1,7 @@
 '''11.  Write a Pandas program to count the duplicate rows of diamonds DataFrame.'''
 import pandas as pd
 
-imp_tab=pd.read_csv("C:/Users/Lenovo/Desktop/diamonds.csv")
+imp_tab=pd.read_csv("https://github.com/RisabhKedai/Spectrum-internship-drive2020-TASK-1/raw/master/diamonds.csv")
 
 lis=list(imp_tab.columns)
 
